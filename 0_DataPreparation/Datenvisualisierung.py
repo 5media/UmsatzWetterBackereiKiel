86d1aa37e1dd9dca0896916bd6457404a16c7a74
5media <167094117+5media@users.noreply.github.com>
2024-05-17 11:34:07 +0000

    new_data

---
+++ 0_DataPreparation/Datenvisualisierung.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Assuming your CSV files are named kiwo.csv, umsatz.csv, and wetter.csv
+
+# Load CSV files into pandas DataFrames
+df1 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/kiwo.csv')
+df2 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/umsatzdaten_gekuerzt.csv')
+df3 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/wetter.csv')
+
+# Print the first few rows of each DataFrame to verify the import
+print("First few rows of kiwo.csv:")
+print(df1.head())
+
+print("\nFirst few rows of umsatz.csv:")
+print(df2.head())
+
+print("\nFirst few rows of wetter.csv:")
+print(df3.head())
+
+
+df3.plot.scatter('Datum', # x-axis
+                'Temperatur', # y-axis
+                grid=True, # Add a grid in the background
+               )
+plt.show()
+
---
+++ 0_DataPreparation/homework_merge_dataframes.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+# Assuming your CSV files are named kiwo.csv, umsatz.csv, and wetter.csv
+
+# Load CSV files into pandas DataFrames
+df1 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/kiwo.csv')
+df2 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/umsatzdaten_gekuerzt.csv')
+df3 = pd.read_csv('/workspaces/test_repo/0_DataPreparation/wetter.csv')
+
+# Print the first few rows of each DataFrame to verify the import
+print("First few rows of kiwo.csv:")
+print(df1.head())
+
+print("\nFirst few rows of umsatz.csv:")
+print(df2.head())
+
+print("\nFirst few rows of wetter.csv:")
+print(df3.head())
+
+# Merge df1 and df2 by 'Datum'
+merged_df = pd.merge(df1, df2, on='Datum')
+
+# Merge the result with df3 by 'Datum'
+merged_df = pd.merge(merged_df, df3, on='Datum')
+
+# Print the resulting merged DataFrame
+print(merged_df)
+
+# Variablen erstellen
+
+# Variable Umsatz-Temperatur
+
+# Variable Umsatz-Wind
+
+# Durchschnitt-Temperatur
+
+# Durchschnitt-Wind
+
+# Durchscnitt-Umsatz
+
+
