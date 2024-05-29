398aaba9b38d7bb3ca7c96f6634764d33e2112ea
5media <167094117+5media@users.noreply.github.com>
2024-05-29 17:30:59 +0200

    WG update

--- 0_DataPreparation/LP_WetterGruppen_v1.py
+++ 0_DataPreparation/LP_WetterGruppen_v1.py
@@ -9,7 +9,7 @@ from Datamerge_local import merged_df
 # Einlesen der merged-Datei 
 df = merged_df
 
-print (merged_df.head())
+# print (merged_df.head())
 
 
 # ------------------------------------------
@@ -30,13 +30,7 @@ labels = ['10-15', '15-20', '20-25', '25-30']
 df_tg=df['TemperaturGruppe'] = pd.cut(df['Temperatur'], bins=bins, labels=labels, include_lowest=True)
 
 # Ausgabe der ersten Zeilen des DataFrame
-print(df_tg.head())
-
-
-
-# ------------------------------------------
-
-
+# print(df_tg.head())
 
 # ------------------------------------------
 
@@ -44,7 +38,7 @@ print(df_tg.head())
 umsatz_je_temperaturgruppe = df.groupby('TemperaturGruppe')['Umsatz'].sum()
 
 # Ausgabe der Ergebnisse
-print(umsatz_je_temperaturgruppe)
+# print(umsatz_je_temperaturgruppe)
 
 # ------------------------------------------
 
@@ -83,6 +77,75 @@ sns.barplot(data=df, x='WindGruppe', y='Umsatz', estimator=sum, ci=None)
 plt.show()
 
 # ------------------------------------------
+
+# Berechnen des maximalen und minimalen Bewölkung
+max_bewoelkung = df['Bewoelkung'].max()
+min_bewoelkung = df['Bewoelkung'].min()
+
+# Ausgabe der Ergebnisse
+print('Maximale Bewoelkung:', max_bewoelkung)
+print('Minimale Bewoelkung:', min_bewoelkung)
+
+# Erstelle ein Balkendiagramm
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df, x='Bewoelkung', y='Umsatz', estimator=sum, ci=None)
+
+# Anzeigen des Plots
+plt.show()
+
+
+# Erstellen der Bewölkungsgruppen
+bins = [-np.inf, 4, 7, np.inf]
+labels = ['0-4', '5-7', '8']
+df['BewoelkungsGruppe'] = pd.cut(df['Bewoelkung'], bins=bins, labels=labels, include_lowest=True)
+
+# Ausgabe der ersten Zeilen des DataFrame
+# print(df.head())
+
+# Summieren des Umsatzes für jede Bewölkungsgruppe
+umsatz_je_bewoelkungsgruppe = df.groupby('BewoelkungsGruppe')['Umsatz'].sum()
+
+# Ausgabe der Ergebnisse
+#print(umsatz_je_bewoelkungsgruppe)
+
+# Erstellen des Balkendiagramms
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df, x='BewoelkungsGruppe', y='Umsatz', estimator=sum, ci=None)
+
+# Anzeigen des Plots
+plt.show()
+
+
+
+# ------------------------------------------
+
+# Summieren des Umsatzes für jeden Wettercode
+umsatz_je_wettercode = df.groupby('Wettercode')['Umsatz'].sum()
+
+# Ausgabe der Ergebnisse
+print(umsatz_je_wettercode)
+
+
+# Zählen der Daten für jeden Wettercode
+daten_je_wettercode = df.groupby('Wettercode').size()
+
+# Ausgabe der Ergebnisse
+print(daten_je_wettercode)
+
+# Ermitteln der Gesamtzahl der Daten
+gesamtzahl_daten = df.shape[0]
+
+# Ausgabe der Ergebnisse
+print('Gesamtzahl der Daten:', gesamtzahl_daten)
+
+
+# erstellen eines Balkendiagramms
+plt.figure(figsize=(10, 6))
+sns.barplot(data=df, x='Wettercode', y='Umsatz', estimator=sum, ci=None)
+
+# Anzeigen des Plots
+plt.show()
+
 
 
 
