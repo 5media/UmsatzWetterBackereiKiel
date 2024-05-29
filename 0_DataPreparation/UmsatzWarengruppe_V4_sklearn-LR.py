86914525a4f8e485919553bccc3eec8c442ba70a
5media <167094117+5media@users.noreply.github.com>
2024-05-29 11:41:11 +0200

    UW v4

--- 0_DataPreparation/UmsatzWarengruppe_V4_sklearn-LR.py
+++ 0_DataPreparation/UmsatzWarengruppe_V4_sklearn-LR.py
@@ -11,7 +11,10 @@ df = merged_df
 
 # -------------------------------------------------------------------------
 
+print ('Scatterplot Temperatur Umsatz Warengruppen')
 sns.scatterplot(x='Temperatur', y='Umsatz', data=df, hue='Warengruppe')
+plt.show()
+
 
 
 xs = df['Temperatur'] .to_numpy().reshape(-1, 1)
@@ -20,9 +23,11 @@ ys = df['Umsatz'].to_numpy()
 
 print ('Temperatur')
 print(xs.shape)
+print ('\n')
 
 print ('Umsatz')
 print(ys.shape)
+print ('\n')
 
 
 from sklearn.linear_model import LinearRegression
@@ -31,13 +36,34 @@ model.fit(xs, ys)
 
 print ('Coef - Intercept')
 print(model.coef_, model.intercept_)
+print ('\n')
 
 # Predict
 def get_umsatz(temperatur):
     return model.coef_ * temperatur + model.intercept_
 
+
+
 print ('Predict Umsatz 30 Grad Celsius')
 print(get_umsatz(30))
+print ('\n')
+# -------------------------------------------------------------------------
 
 
 
+# Erstellen des OLS Modells
+ols_model = LinearRegression()
+
+# Anpassen des Modells an die Daten
+ols_model.fit(xs, ys)
+
+# Ausgabe der Koeffizienten und des Achsenabschnitts
+print('Koeffizienten:', ols_model.coef_)
+print('Achsenabschnitt:', ols_model.intercept_)
+
+# Erstellen des Scatterplots
+plt.scatter(xs, ys, color='blue')
+plt.plot(xs, ols_model.predict(xs), color='red')
+plt.xlabel('Temperatur')
+plt.ylabel('Umsatz')
+plt.show()
