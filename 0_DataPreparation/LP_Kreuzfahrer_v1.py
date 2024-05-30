5cdbb5d908318385b324c10a5dedadc6510c9964
5media <167094117+5media@users.noreply.github.com>
2024-05-30 12:47:25 +0200

    kreuzfahrer

--- 0_DataPreparation/LP_Kreuzfahrer_v1.py
+++ 0_DataPreparation/LP_Kreuzfahrer_v1.py
@@ -15,9 +15,29 @@ df_kreuzfahrer = pd.read_csv('kiel_kreuzfahrer.csv')
 
 print (df_kreuzfahrer.head())
 
-df_kreuzfahrer['Jahr'] = pd.to_datetime(df_kreuzfahrer['Jahr'], format='%Y')
+# df_kreuzfahrer['Jahr'] = pd.to_datetime(df_kreuzfahrer['Jahr'], format='%Y')
+# print (df_kreuzfahrer)
 
 
-print (df_kreuzfahrer)
+# Stellen Sie sicher, dass 'Datum' im datetime Format ist
+df['Datum'] = pd.to_datetime(df['Datum'])
+
+# Erstellen Sie eine neue Spalte 'Jahr', die das Jahr aus 'Datum' extrahiert
+df['Jahr'] = df['Datum'].dt.year
+
+print (df.head())
+
+# merge df_kreuzfahrer with df
+df = pd.merge(df, df_kreuzfahrer, on='Jahr', how='inner')
+
+print (df.head())
+
+import matplotlib.pyplot as plt
+
+# Gruppieren Sie das DataFrame nach 'Jahr' und summieren Sie 'Umsatz' und 'Kreuzfahrer'
+# df_sum = df.groupby('Jahr')[['Umsatz', 'Kreuzfahrer']].sum().reset_index()
+
+
+
 
 
