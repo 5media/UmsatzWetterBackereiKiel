0cbcb19a5a0e26c03a48f71944abdf4416c8b02c
FlorinevdL <167337587+FlorinevdL@users.noreply.github.com>
2024-06-20 21:12:27 +0000

    Bearbeitung_csv_Passagieraufkommen

--- 0_DataPreparation/Ordnungsamt Kiel/Datenaufbereitung_csv.py
+++ 0_DataPreparation/Ordnungsamt Kiel/Datenaufbereitung_csv.py
@@ -10,11 +10,20 @@ df = pd.read_excel(Passagieraufkommen)
 csv_datei = 'Passagieraufkommen.csv'
 df.to_csv(csv_datei, index=False)
 
+## Überprüfung der CSV-Datei
 # Pfad zur CSV-Datei
 Passagieraufkommen_csv = ('/workspaces/UmsatzWetterBackereiKiel/0_DataPreparation/Ordnungsamt Kiel/CSV/Passagieraufkommen.csv')
-
 # Lese die CSV-Datei ein
 df_Passagieraufkommen = pd.read_csv(Passagieraufkommen_csv)
-
 # Zeige die Spaltenüberschriften an
 print(df_Passagieraufkommen.columns)
+
+columns_to_replace = ['pas_kreuzfahrt_absolut', 'pas_kreuzfahrt_relativ', 'anzahl_Kreuzfahrtschiff_absolut']
+df_Passagieraufkommen.loc[0, columns_to_replace] = df.loc[1, columns_to_replace].values
+
+# Zeige den aktualisierten Dataframe an
+print(df_Passagieraufkommen.head())
+
+# Speichere den aktualisierten DataFrame als CSV-Datei
+aktualisierte_csv_datei = 'Passagieraufkommen_neu.csv'
+df_Passagieraufkommen.to_csv(aktualisierte_csv_datei, index=False)
